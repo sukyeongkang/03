c415468999f5ca86e2e83404f6f1f7b4343219e8
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-09-20 17:42:22 +0900

    4

---
+++ main4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a,b;
+	
+	
+	printf("분자를 입력하세요:");
+	scanf("%d",&a);
+	printf("분모를 입력하세요:",b);
+	scanf("%d",&b);
+	
+	
+	printf("나누기의 결과 값은 %f 입니다.\n",(float)a/b);
+
+	
+	return 0;
+}
