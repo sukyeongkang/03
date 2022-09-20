0180ba60fbbaa9e53b138e3dcb5c4ae1297d2909
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-09-20 17:21:31 +0900

    1

---
+++ main2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+   char text;
+   
+   printf("input a character :");
+   scanf("%c", &text);
+   
+   printf("the text of character of %c (%d) is %c (%d)\n", text, text, text+1);
+   
+	
+	
+   return 0;
+}
