c3c0b612bcd583c3286ec93658e43deed10f3671
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-09-20 19:35:18 +0900

    2

--- main2.c
+++ main2.c
@@ -4,15 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	char c;
+	int i;
+	
+	printf("input a number :");
+	scanf("%c", &c);
+	
+	i=c-'0';
+	
+	printf("The input number is %i\n", i);
+	
 
-   char text;
-   
-   printf("input a character :");
-   scanf("%c", &text);
-   
-   printf("the text of character of %c (%d) is %c (%d)\n", text, text,text+1, text+1);
-   
-	
-	
-   return 0;
+	return 0;
 }
